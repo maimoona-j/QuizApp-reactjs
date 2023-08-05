19e68a090a1c4f3bd1b3a56b8c7c879ad333cb89
maimoona <maimoonaj97@gmail.com>
2023-08-05 21:17:57 +0500

    commit

--- src/Components/QuizDetail.jsx
+++ src/Components/QuizDetail.jsx
@@ -1,3 +1,234 @@
+// import React, { useState } from "react";
+// import QuizQuestion from "../Components/QuizQuestion";
+// import QuizResult from "../Components/QuizResult";
+
+// export default function QuizDetail({
+//   questions,
+//   userAnswers,
+//   setUserAnswers,
+//   setShowQuiz,
+// }) {
+//   const [selectedOption, setSelectedOption] = useState("all");
+//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+//   const [showResult, setShowResult] = useState(false);
+
+//   const handleSelected = (e) => {
+//     setSelectedOption(e.target.value);
+//   };
+
+//   const handleNext = () => {
+//     if (currentQuestionIndex < QuizQuestion.length - 1) {
+//       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+//       setSelectedOption("");
+//     }
+//   };
+
+//   const handleBack = () => {
+//     if (currentQuestionIndex > 1) {
+//       setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+//       setSelectedOption("");
+//     }
+//   };
+
+//   const handleSubmit = () => {
+//     setUserAnswers((prevAnswers) => ({
+//       ...prevAnswers,
+//       [questions[currentQuestionIndex].id]: selectedOption,
+//     }));
+
+//     if (currentQuestionIndex === questions.length - 1) {
+//       setShowQuiz(false); // Switch to the result view when submitting the quiz
+//     } else {
+//       handleNext();
+//     }
+//   };
+
+//   const currentQuestion = QuizQuestion[currentQuestionIndex];
+//   const isLastQuestion = currentQuestionIndex === QuizQuestion.length - 1;
+//   return (
+//     <div>
+//       <h1 className="text-center text-2xl font-bold lg:mt-3 mt-5">
+//         JavaScript Quiz
+//       </h1>
+
+//       {showResult ? (
+//         <QuizResult />
+//       ) : (
+//         <div className="border border-blue-600 w-[300px] lg:w-[800px] lg:h-[450px] h-[480px] ml-11 lg:ml-[330px] mt-24 p-8 rounded shadow-lg">
+//           <h3 className="font-bold">{currentQuestion.question}</h3>
+//           <p className="text-xl mt-2">{currentQuestion.text}</p>
+
+//           <div className="flex flex-col lg:mt-4 mt-3  mr-12 ">
+//             {currentQuestion.options.map((option) => (
+//               <label
+//                 key={option.id}
+//                 htmlFor={option.id}
+//                 className="flex items-center mt-2"
+//               >
+//                 <input
+//                   type="radio"
+//                   // checked={selectedOption === option.id}
+//                   name="sort"
+//                   value={option.id}
+//                   checked={selectedOption === option.id}
+//                   onChange={handleSelected}
+//                   className="mr-1"
+//                 />
+//                 {option.text}
+//               </label>
+//             ))}
+//           </div>
+
+//           <div className="lg:mt-16 mt-8 lg:ml-[200px] ml-8 absolute lg:bottom-20 bottom-[25%] left-[20%] lg:left-[25%] right-0">
+//             {/* Use "absolute" positioning to fix the buttons at the bottom */}
+//             <button
+//               className="bg-blue-300 hover:bg-gray-300 font-bold p-3 lg:w-36 w-16 rounded"
+//               onClick={handleBack}
+//               disabled={currentQuestionIndex === 0}
+//             >
+//               Back
+//             </button>
+//             {isLastQuestion ? (
+//               <button
+//                 className="bg-green-300 hover:bg-gray-300 font-bold p-3 lg:w-36 w-24 ml-3 rounded"
+//                 onClick={handleSubmit}
+//               >
+//                 Submit
+//               </button>
+//             ) : (
+//               <button
+//                 className="bg-blue-300 font-bold hover:bg-gray-300 p-3 lg:w-36 w-16 ml-3 rounded"
+//                 onClick={handleNext}
+//               >
+//                 Next
+//               </button>
+//             )}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+
+
+
+
+// import React, { useState } from 'react';
+// import QuizQuestion from '../Components/QuizQuestion';
+// import QuizResult from '../Components/QuizResult';
+
+// export default function QuizDetail({ setShowQuiz, setCorrectAnswers, setWrongAnswers }) {
+//   const [selectedOption, setSelectedOption] = useState('');
+//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+//   const [showResult, setShowResult] = useState(false);
+
+//   const handleSelected = (e) => {
+//     setSelectedOption(e.target.value);
+//   };
+
+//   const handleNext = () => {
+//     if (currentQuestionIndex < QuizQuestion.length - 1) {
+//       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+//       setSelectedOption("");
+//     }
+//   };
+
+//   const handleBack = () => {
+//     if (currentQuestionIndex > 1) {
+//       setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+//       setSelectedOption("");
+//     }
+//   };
+
+//   const handleSubmit = () => {
+//     // Calculate the number of correct and wrong answers based on the user's responses
+//     const currentQuestion = QuizQuestion[currentQuestionIndex];
+//     const selectedAnswer = currentQuestion.options.find((option) => option.id === selectedOption);
+//     if (selectedAnswer && selectedAnswer.isCorrect) {
+//       setCorrectAnswers((prevCorrectAnswers) => prevCorrectAnswers + 1);
+//     } else {
+//       setWrongAnswers((prevWrongAnswers) => prevWrongAnswers + 1);
+//     }
+
+//     // Move to the next question or show the result when all questions are answered
+//     if (currentQuestionIndex < QuizQuestion.length - 1) {
+//       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+//       setSelectedOption('');
+//     } else {
+//       setShowQuiz(false);
+//     }
+//   };
+
+//   const currentQuestion = QuizQuestion[currentQuestionIndex];
+//   const isLastQuestion = currentQuestionIndex === QuizQuestion.length - 1;
+//   return (
+//     <div>
+//       <h1 className="text-center text-2xl font-bold lg:mt-3 mt-5">
+//         JavaScript Quiz
+//       </h1>
+
+//       {showResult ? (
+//         <QuizResult />
+//       ) : (
+//         <div className="border border-blue-600 w-[300px] lg:w-[800px] lg:h-[450px] h-[480px] ml-11 lg:ml-[330px] mt-24 p-8 rounded shadow-lg">
+//           <h3 className="font-bold">{currentQuestion.question}</h3>
+//           <p className="text-xl mt-2">{currentQuestion.text}</p>
+
+//           <div className="flex flex-col lg:mt-4 mt-3  mr-12 ">
+//             {currentQuestion.options.map((option) => (
+//               <label
+//                 key={option.id}
+//                 htmlFor={option.id}
+//                 className="flex items-center mt-2"
+//               >
+//                 <input
+//                   type="radio"
+//                   // checked={selectedOption === option.id}
+//                   name="sort"
+//                   value={option.id}
+//                   checked={selectedOption === option.id}
+//                   onChange={handleSelected}
+//                   className="mr-1"
+//                 />
+//                 {option.text}
+//               </label>
+//             ))}
+//           </div>
+
+//           <div className="lg:mt-16 mt-8 lg:ml-[200px] ml-8 absolute lg:bottom-20 bottom-[25%] left-[20%] lg:left-[25%] right-0">
+//             {/* Use "absolute" positioning to fix the buttons at the bottom */}
+//             <button
+//               className="bg-blue-300 hover:bg-gray-300 font-bold p-3 lg:w-36 w-16 rounded"
+//               onClick={handleBack}
+//               disabled={currentQuestionIndex === 0}
+//             >
+//               Back
+//             </button>
+//             {isLastQuestion ? (
+//               <button
+//                 className="bg-green-300 hover:bg-gray-300 font-bold p-3 lg:w-36 w-24 ml-3 rounded"
+//                 onClick={handleSubmit}
+//               >
+//                 Submit
+//               </button>
+//             ) : (
+//               <button
+//                 className="bg-blue-300 font-bold hover:bg-gray-300 p-3 lg:w-36 w-16 ml-3 rounded"
+//                 onClick={handleNext}
+//               >
+//                 Next
+//               </button>
+//             )}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+
+
 import React, { useState } from "react";
 import QuizQuestion from "../Components/QuizQuestion";
 import QuizResult from "../Components/QuizResult";
@@ -109,3 +340,152 @@ export default function QuizDetail({
     </div>
   );
 }
+
+
+
+
+
+// import React, { useState } from "react";
+// import QuizQuestion from "../Components/QuizQuestion";
+// import QuizResult from "../Components/QuizResult";
+
+// export default function QuizDetail({
+//   questions,
+//   userAnswers,
+//   setUserAnswers,
+//   setShowQuiz,
+// }) {
+//   const [selectedOption, setSelectedOption] = useState("all");
+//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+//   const [showResult, setShowResult] = useState(false);
+
+//   const handleSelected = (e) => {
+//     setSelectedOption(e.target.value);
+//   };
+
+//   const handleNext = () => {
+//     if (currentQuestionIndex < QuizQuestion.length - 1) {
+//       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+//       setSelectedOption("");
+//     }
+//   };
+
+//   const handleBack = () => {
+//     if (currentQuestionIndex > 1) {
+//       setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+//       setSelectedOption("");
+//     }
+//   };
+
+//   // const handleAnswerSelect = (questionId, selectedOptionId) => {
+//   //   setSelectedOption(selectedOptionId);
+//   // };
+
+//   const handleAnswerSelect = (questionId, selectedOptionId) => {
+//     setUserAnswers((prevAnswers) => ({
+//       ...prevAnswers,
+//       [questionId]: selectedOptionId,
+//     }));
+//     setSelectedOption(selectedOptionId);
+//   };
+
+
+//   const handleSubmit = () => {
+//     if (selectedOption !== null) {
+//       setUserAnswers((prevAnswers) => ({
+//         ...prevAnswers,
+//         [questions[currentQuestionIndex].id]: selectedOption,
+//       }));
+//     }
+  
+//     if (currentQuestionIndex === questions.length - 1) {
+//       setShowResult(true); // Switch to the result view when submitting the quiz
+//     } else {
+//       handleNext();
+//     }
+//   };
+  
+  
+// //  const handleSubmit = () => {
+// //     setUserAnswers((prevAnswers) => ({
+// //       ...prevAnswers,
+// //       [questions[currentQuestionIndex].id]: selectedOption,
+// //     }));
+
+// //     if (currentQuestionIndex === questions.length - 1) {
+// //       console.log("userAnswers in QuizDetail:", userAnswers);
+
+// //       setShowQuiz(false); // Switch to the result view when submitting the quiz
+// //     } else {
+// //       handleNext();
+// //     }
+// //   };
+
+//   const currentQuestion = QuizQuestion[currentQuestionIndex];
+//   const isLastQuestion = currentQuestionIndex === QuizQuestion.length - 1;
+//   return (
+//     <div>
+//       <h1 className="text-center text-2xl font-bold lg:mt-3 mt-5">
+//         JavaScript Quiz
+//       </h1>
+
+//       {showResult ? (
+//         <QuizResult />
+//       ) : (
+//         <div className="border border-blue-600 w-[300px] lg:w-[800px] lg:h-[450px] h-[480px] ml-11 lg:ml-[330px] mt-24 p-8 rounded shadow-lg">
+//           <h3 className="font-bold">{currentQuestion.question}</h3>
+//           <p className="text-xl mt-2">{currentQuestion.text}</p>
+
+//           <div className="flex flex-col lg:mt-4 mt-3  mr-12 ">
+//             {currentQuestion.options.map((option) => (
+//               <label
+//                 key={option.id}
+//                 htmlFor={option.id}
+//                 className="flex items-center mt-2"
+//               >
+//                 <input
+//                   type="radio"
+//                   // checked={selectedOption === option.id}
+//                   name="sort"
+//                   value={option.id}
+//                   checked={selectedOption === option.id}
+//                   // onChange={handleSelected}
+//                   onChange={() => handleAnswerSelect(questions[currentQuestionIndex].id, option.id)}
+//                   className="mr-1"
+//                 />
+//                 {option.text}
+//               </label>
+//             ))}
+//           </div>
+
+//           <div className="lg:mt-16 mt-8 lg:ml-[200px] ml-8 absolute lg:bottom-20 bottom-[25%] left-[20%] lg:left-[25%] right-0">
+//             {/* Use "absolute" positioning to fix the buttons at the bottom */}
+//             <button
+//               className="bg-blue-300 hover:bg-gray-300 font-bold p-3 lg:w-36 w-16 rounded"
+//               onClick={handleBack}
+//               disabled={currentQuestionIndex === 0}
+//             >
+//               Back
+//             </button>
+//             {isLastQuestion ? (
+//               <button
+//                 className="bg-green-300 hover:bg-gray-300 font-bold p-3 lg:w-36 w-24 ml-3 rounded"
+//                 onClick={handleSubmit}
+//               >
+//                 Submit
+//               </button>
+//             ) : (
+//               <button
+//                 className="bg-blue-300 font-bold hover:bg-gray-300 p-3 lg:w-36 w-16 ml-3 rounded"
+//                 onClick={handleNext}
+//               >
+//                 Next
+//               </button>
+//             )}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -1,7 +1,74 @@
-import React, {useState} from 'react'
-import QuizQuestions from '../Components/QuizQuestion';
-import QuizDetail from '../Components/QuizDetail'
-import QuizResult from '../Components/QuizResult'
+// import React, {useState} from 'react'
+// import QuizQuestions from '../Components/QuizQuestion';
+// import QuizDetail from '../Components/QuizDetail'
+// import QuizResult from '../Components/QuizResult'
+
+// export default function Quiz() {
+//   const [showQuiz, setShowQuiz] = useState(true);
+//   const [userAnswers, setUserAnswers] = useState({});
+
+//   const handleRestart = () => {
+//     // Reset the state to show the quiz again
+//     setShowQuiz(true);
+//     setUserAnswers({});
+//   };
+//   return (
+
+//     <div>
+//   {showQuiz ? (
+//         // Show the QuizDetail component if showQuiz is true
+//         <QuizDetail 
+//         question={QuizQuestions}
+//         userAnswers={userAnswers}
+//         setUserAnswers={setUserAnswers}
+//         setShowQuiz={setShowQuiz}
+//         />
+//       ) : (
+//         // Show the QuizResult component if showQuiz is false
+//         <QuizResult
+//         question={QuizQuestions} 
+//         userAnswers={userAnswers} 
+//          handleRestart={handleRestart}
+//           />
+//       )}
+
+//     </div>
+//   )
+// }
+
+
+  
+// import React, { useState } from 'react';
+// import QuizDetail from '../Components/QuizDetail';
+// import QuizResult from '../Components/QuizResult';
+
+// export default function Quiz() {
+//   const [showQuiz, setShowQuiz] = useState(true);
+//   const [correctAnswers, setCorrectAnswers] = useState(0);
+//   const [wrongAnswers, setWrongAnswers] = useState(0);
+
+//   const handleRestart = () => {
+//     setShowQuiz(true);
+//     setCorrectAnswers(0);
+//     setWrongAnswers(0);
+//   };
+
+//   return (
+//     <div>
+//       {showQuiz ? (
+//         <QuizDetail setShowQuiz={setShowQuiz} setCorrectAnswers={setCorrectAnswers} setWrongAnswers={setWrongAnswers} />
+//       ) : (
+//         <QuizResult correctAnswers={correctAnswers} wrongAnswers={wrongAnswers} handleRestart={handleRestart} />
+//       )}
+//     </div>
+//   );
+// }
+
+
+import React, { useState } from "react";
+import QuizQuestions from "../Components/QuizQuestion";
+import QuizDetail from "../Components/QuizDetail";
+import QuizResult from "../Components/QuizResult";
 
 export default function Quiz() {
   const [showQuiz, setShowQuiz] = useState(true);
@@ -12,29 +79,26 @@ export default function Quiz() {
     setShowQuiz(true);
     setUserAnswers({});
   };
-  return (
 
+  return (
     <div>
-  {showQuiz ? (
+      {showQuiz ? (
         // Show the QuizDetail component if showQuiz is true
-        <QuizDetail 
-        questions={QuizQuestions}
-        userAnswers={userAnswers}
-        setUserAnswers={setUserAnswers}
-        setShowQuiz={setShowQuiz}
+        <QuizDetail
+          questions={QuizQuestions} // Passing the question prop here
+          userAnswers={userAnswers}
+          setUserAnswers={setUserAnswers}
+          setShowQuiz={setShowQuiz}
         />
       ) : (
         // Show the QuizResult component if showQuiz is false
         <QuizResult
-        questions={QuizQuestions} 
-        userAnswers={userAnswers} 
-         handleRestart={handleRestart}
-          />
+          questions={QuizQuestions} // Corrected the prop name to "questions"
+          userAnswers={userAnswers}
+          handleRestart={handleRestart}
+        />
       )}
-
     </div>
-  )
+  );
 }
 
-
- 
